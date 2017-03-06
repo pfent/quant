4f8b8af10770d8fac16ca38b6fe62583b285207a
Travis CI <travis@travis-ci.org>
2017-03-06 13:18:12 +0000

    Update doxygen documentation on branch gh-pages.
    Travis CI build 15.
    
    Commit: d967518e780c23fc57b57e0e367f5d5e165dcc82.

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['calc_5foff_5flen',['calc_off_len',['../frame_8c.html#a1cc580cb180f77e63292b87f2edc9a78',1,'frame.c']]],
-  ['calc_5freq_5fpkt_5fnr_5flen',['calc_req_pkt_nr_len',['../pkt_8c.html#a2b4a333a6bbb6e39ffff9da22494498f',1,'calc_req_pkt_nr_len(const uint64_t n):&#160;pkt.h'],['../pkt_8h.html#a2b4a333a6bbb6e39ffff9da22494498f',1,'calc_req_pkt_nr_len(const uint64_t n):&#160;pkt.h']]],
+  ['calc_5freq_5fpkt_5fnr_5flen',['calc_req_pkt_nr_len',['../pkt_8c.html#ae965f076c1a2e3e01db61e979e8061cd',1,'calc_req_pkt_nr_len(uint64_t n):&#160;pkt.h'],['../pkt_8h.html#a2b4a333a6bbb6e39ffff9da22494498f',1,'calc_req_pkt_nr_len(const uint64_t n):&#160;pkt.h']]],
   ['calc_5fsid_5flen',['calc_sid_len',['../frame_8c.html#a09b918fe9ce60943cb2730bc4d8ecb92',1,'frame.c']]],
   ['check_5fconn',['check_conn',['../quic_8c.html#a3092586873d00e4ffb58a29ccd5b8be8',1,'quic.c']]],
   ['check_5fstream',['check_stream',['../quic_8c.html#a87cbf45a6ea3712344313d61e92eef9f',1,'quic.c']]],
