6222dd721d770a8f63303cefcda47b8292f287d6
Travis CI <travis@travis-ci.org>
2018-10-31 11:18:12 +0000

    Update doxygen documentation on branch gh-pages.
    Travis CI build 695.
    
    Commit: 6784476b828aa2f8685f9cbd29b566d65b9cc484.

--- search/defines_8.js
+++ search/defines_8.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['have_5fasan',['HAVE_ASAN',['../config_8h.html#a08ee9e2d328d1d579ee81843a3b01692',1,'config.h']]],
   ['have_5fendian_5fh',['HAVE_ENDIAN_H',['../config_8h.html#a9ef8e61ca569fc24c3e47aeab03498c6',1,'config.h']]],
-  ['have_5fspace_5ffor',['have_space_for',['../pkt_8c.html#a46ce9d2463ab11f297d94c53f5e194ac',1,'pkt.c']]],
   ['hex2str',['hex2str',['../quic_8h.html#a0721d3a6a0adbe5443a85d94ee4da000',1,'quic.h']]],
   ['hkdf_5fbase_5flabel',['HKDF_BASE_LABEL',['../tls_8c.html#a62a6e8160885163bbf0d7c412635752c',1,'tls.c']]]
 ];
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['have_5fspace_5ffor',['have_space_for',['../pkt_8c.html#a2971ac1a1c667293ba2407d9a0050e99',1,'pkt.c']]],
   ['hshk_5fpkts_5foutstanding',['hshk_pkts_outstanding',['../recovery_8c.html#a0ab224b867c365ffdb3150b57d836ee8',1,'recovery.c']]]
 ];
