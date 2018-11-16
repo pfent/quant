e9c13508777996400fd4c09b6620e29970dc534c
Travis CI <travis@travis-ci.org>
2018-11-16 09:00:31 +0000

    Update doxygen documentation on branch gh-pages.
    Travis CI build 745.
    
    Commit: 7a944e1ed6f07fa3437da3e6fc6cc4df7762c499.

--- search/functions_11.js
+++ search/functions_11.js
@@ -6,11 +6,11 @@ var searchData=
   ['track_5fbytes_5fin',['track_bytes_in',['../stream_8c.html#a551a7c19bf90417d819623804da6949f',1,'track_bytes_in(struct q_stream *const s, const uint64_t n):&#160;stream.c'],['../stream_8h.html#a551a7c19bf90417d819623804da6949f',1,'track_bytes_in(struct q_stream *const s, const uint64_t n):&#160;stream.c']]],
   ['track_5fbytes_5fout',['track_bytes_out',['../stream_8c.html#a0c28462a470893c1046d7a167037d926',1,'track_bytes_out(struct q_stream *const s, const uint64_t n):&#160;stream.c'],['../stream_8h.html#a0c28462a470893c1046d7a167037d926',1,'track_bytes_out(struct q_stream *const s, const uint64_t n):&#160;stream.c']]],
   ['trim_5fframe',['trim_frame',['../frame_8c.html#ae6b33881d5310ee374d246e582265d43',1,'frame.c']]],
-  ['tx',['tx',['../conn_8c.html#abc878d206fec5787bb6754f6b53d0e8a',1,'tx(struct q_conn *const c, const bool rtx, const uint32_t limit):&#160;conn.c'],['../conn_8h.html#abc878d206fec5787bb6754f6b53d0e8a',1,'tx(struct q_conn *const c, const bool rtx, const uint32_t limit):&#160;conn.c']]],
+  ['tx',['tx',['../conn_8c.html#a1a9a96efc3be558373804c33a60df6b9',1,'tx(struct q_conn *const c, const uint32_t limit):&#160;conn.c'],['../conn_8h.html#a1a9a96efc3be558373804c33a60df6b9',1,'tx(struct q_conn *const c, const uint32_t limit):&#160;conn.c']]],
   ['tx_5fack',['tx_ack',['../conn_8c.html#a87cb12b0afa8cdb9f2a41ce602d8ff96',1,'tx_ack(struct q_conn *const c, const epoch_t e):&#160;conn.c'],['../conn_8h.html#a87cb12b0afa8cdb9f2a41ce602d8ff96',1,'tx_ack(struct q_conn *const c, const epoch_t e):&#160;conn.c']]],
   ['tx_5fcrypto',['tx_crypto',['../conn_8c.html#ac0d0edf08b6c3f2b3a75ab87498def1f',1,'conn.c']]],
   ['tx_5fstream_5fctrl',['tx_stream_ctrl',['../conn_8c.html#a7bc1cf055ddc94c793f81fdfc52ecdbb',1,'conn.c']]],
-  ['tx_5fstream_5fdata',['tx_stream_data',['../conn_8c.html#a1a39c653f20bef724b56574fac9c449f',1,'conn.c']]],
+  ['tx_5fstream_5fdata',['tx_stream_data',['../conn_8c.html#aa07824d29034256950ac9380e9934ca3',1,'conn.c']]],
   ['tx_5fvneg_5fresp',['tx_vneg_resp',['../pkt_8c.html#acda77940d975cd06aecc7f0aae9f2aa0',1,'tx_vneg_resp(const struct w_sock *const ws, const struct w_iov *const v):&#160;pkt.c'],['../pkt_8h.html#acda77940d975cd06aecc7f0aae9f2aa0',1,'tx_vneg_resp(const struct w_sock *const ws, const struct w_iov *const v):&#160;pkt.c']]],
   ['tx_5fw',['tx_w',['../conn_8c.html#ae7ad1167799a2920119487df92e81b85',1,'tx_w(struct ev_loop *const l, ev_async *const w, int e):&#160;conn.c'],['../conn_8h.html#ae7ad1167799a2920119487df92e81b85',1,'tx_w(struct ev_loop *const l, ev_async *const w, int e):&#160;conn.c']]]
 ];
